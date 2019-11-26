5fafeb6a733be9e0ff2ecd99f8b0cae194651286
Siddhant Rai <35813591+siddhant230@users.noreply.github.com>
2019-11-26 19:46:34 +0530

    Update Q_TABLE_CREATOR.py

--- Q_TABLE_CREATOR.py
+++ Q_TABLE_CREATOR.py
@@ -3,6 +3,9 @@ import numpy as np
 
 size=10
 q_table={}
+##here x1,x2,x3,x4 represents your required feature space
+##for example for moving in a grid only four movements are allowed=>UP DOWN LEFT RIGHT. So, x1,x2,x3,x4 to cover all valid combinations.
+##another example can be flappy bird...required feature space is either UP or DOWN. So, x1,x2.
 for x1 in range(-size+1,size):
     for x2 in range(-size+1,size):
         for y1 in range(-size+1,size):
